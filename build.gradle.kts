9944beeb175e1427f09f597b79dc9b4433c22cf5
MAAC <49979494+MohammadAbdoulAzizCiss@users.noreply.github.com>
2024-01-25 10:48:14 +0000

    adding actuator and micrometer dependencies for monitoring purpose

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	// https://mvnrepository.com/artifact/io.micrometer/micrometer-registry-prometheus
+	implementation("io.micrometer:micrometer-registry-prometheus:1.12.2")
+	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-actuator
+	implementation("org.springframework.boot:spring-boot-starter-actuator:3.2.2")
 	compileOnly("org.projectlombok:lombok")
   	developmentOnly("org.springframework.boot:spring-boot-devtools")
  	runtimeOnly("com.mysql:mysql-connector-j")	
